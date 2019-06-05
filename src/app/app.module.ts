8269266a880f1dab96bdcff7e94c722fa7b3e8ab
Paulo Tejada <paulo@MacBook-Pro-de-Paulo.local>
2019-06-04 22:39:50 -0500

    build base router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+import { HttpClientModule } from '@angular/common/http'
+
+
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
+
+//Importar Rutas
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot( ROUTES, { useHash:true } )
   ],
   providers: [],
   bootstrap: [AppComponent]
